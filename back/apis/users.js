9793cba91b1f98b3aec8ad1498a49f9838a0a302
changicho <changicho29@gmail.com>
2020-07-06 21:14:02 +0900

    feat: Add init files of project
    ?: 기본 세팅
    - create react app
    - express generator
    eslint 기본만 적용 (airbnb x)

---
+++ back/apis/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res) {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
