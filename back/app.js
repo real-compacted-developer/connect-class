e27d2984c15ed08bb7394baf91321eefce753517
combbm <combbm@gmail.com>
2020-07-28 18:20:22 +0900

    fix: Delete unused css file
    fix: Delete unused express static code

--- back/app.js
+++ back/app.js
@@ -1,6 +1,5 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -12,7 +11,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", usersRouter);
 
