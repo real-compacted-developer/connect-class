b2346e251701f010fd1f4be1e781027db9c99f2c
combbm <combbm@gmail.com>
2020-08-03 17:18:23 +0900

    comment: Delete unused comments
    ?: because it can understand without comment

--- back/app.js
+++ back/app.js
@@ -14,18 +14,14 @@ app.use(cookieParser());
 
 app.use("/", usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
