36b01a48cce79836cef19a68f897d8948039c936
combbm <combbm@gmail.com>
2020-08-04 19:33:43 +0900

    feat: Add broadcast code in draw event of socket
    ?: draw(필기) 기능 구현을 위해 추가

--- back/sockets/canvas.js
+++ back/sockets/canvas.js
@@ -2,7 +2,6 @@ module.exports = function (io) {
   const SOCKET_TYPE = require("../constants/socket-type");
 
   io.on(SOCKET_TYPE.DRAW, (data) => {
-    console.log(data);
-    // TODO: 필기 관련 소켓
+    io.broadcast.emit(SOCKET_TYPE.DRAW, data);
   });
 };
