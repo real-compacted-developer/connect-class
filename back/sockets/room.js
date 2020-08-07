707513a900600ac3ebf0fd86944cfc6f22cce3bb
combbm <combbm@gmail.com>
2020-08-07 17:29:51 +0900

    feat: Add create room and delete room sockets
    ?: 방 생성, 삭제 소켓 통신 구현
    Database Model 작성이 안 되어있기 때문에 해당 부분 보류

--- back/sockets/room.js
+++ back/sockets/room.js
@@ -1,5 +1,25 @@
 module.exports = function (io) {
   const SOCKET_TYPE = require("../constants/socket-type");
+  const Room = require("../app").RoomInstance;
+
+  io.on(SOCKET_TYPE.CREATE_ROOM, (data) => {
+    // const createRoom = RoomModel.insert(data);
+    const id = 1; // TODO: DB Model 추가 후 DB에서 id 가져오기
+
+    Room.addRoom(id, data);
+
+    console.log(`새로운 방 생성: ${JSON.stringify(data)}`);
+
+    io.emit(SOCKET_TYPE.CREATE_ROOM, data);
+  });
+
+  io.on(SOCKET_TYPE.DELETE_ROOM, (data) => {
+    const deleteRoom = Room.deleteRoom(data);
+    
+    console.log(`방 삭제: ${JSON.stringify(deleteRoom)}`);
+
+    io.emit(SOCKET_TYPE.DELETE_ROOM, deleteRoom);
+  });
 
   io.on(SOCKET_TYPE.JOIN, (data) => {
     // TODO: 방 입장
