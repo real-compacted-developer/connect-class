6c5e51620ebff1789cf31ef3344180f32ad81bae
combbm <combbm@gmail.com>
2020-08-07 21:17:42 +0900

    feat: Add addUser() and deleteUser() functions
    ?: 스터디룸의 참가 유저를 관리하기 위해 추가

--- back/stores/RoomStore.js
+++ back/stores/RoomStore.js
@@ -26,6 +26,17 @@ class RoomStore {
     this.roomList[roomId] = data;
     return this.roomList[roomId];
   }
+
+  addUser(roomId, userId, userData) {
+    this.roomList[roomId].users[userId] = userData;
+    return this.roomList[roomId].users[userId];
+  }
+
+  deleteUser(roomId, userId) {
+    const tempUser = this.roomList[roomId].users[userId];
+    delete this.roomList[roomId].users[userId];
+    return tempUser;
+  }
 }
 
 module.exports = RoomStore;
