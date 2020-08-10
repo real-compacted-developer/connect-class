c15c16d5a23d725286370314aa1ae7b8e75d29e2
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-08-10 17:58:28 +0900

    feat[BACK]: image slide 기능을 위한 socket 기능 추가
    ## 전역 변수
    ### imagesPath, size
    - 스터디에 쓰이는 pdf를 자른 image들
    - 일단 더미데이터로 사용
    - 나중에 DB 연동해서 사용할 예정
    
    ## imageChange
    - 특정 room(스터디그룹)에게 보여지는 페이지가 모두 같아지게 하려고 emit을 보낸다.
    - io를 안쓰고 socket을 사용하고 싶은데 방법을 몰라서 일단 io로 구현

--- back/sockets/image.js
+++ back/sockets/image.js
@@ -1,10 +1,31 @@
-module.exports = function (io) {
+const imagesPath = [
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat1.jpg",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat2.jpg",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat3.jpg",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/puppy1.jpg"
+];
+const size = imagesPath.length;
+module.exports = function (socket) {
   const SOCKET_TYPE = require("../constants/socket-type");
 
-  io.on(SOCKET_TYPE.IMAGE_PREV, (data) => {
+  socket.on(SOCKET_TYPE.IMAGE_PREV, (data) => {
     // TODO: 자료 이전으로 넘기기
+    if (data.index == 0) {
+      data.index = size - 1;
+    } else {
+      data.index -= 1;
+    }
+    data.urlInfo = imagesPath[data.index];
+    io.in("roomNumber").emit("imageChange", data);
   });
-  io.on(SOCKET_TYPE.IMAGE_NEXT, (data) => {
+  socket.on(SOCKET_TYPE.IMAGE_NEXT, (data) => {
     // TODO: 자료 다음으로 넘기기
+    if (data.index >= size - 1) {
+      data.index = 0;
+    } else {
+      data.index += 1;
+    }
+    data.urlInfo = imagesPath[data.index];
+    io.in("roomNumber").emit("imageChange", data);
   });
 };
