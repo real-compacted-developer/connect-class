63e1d7f144ee06a2339cecb9fdcdfb5893b6019b
combbm <combbm@gmail.com>
2020-08-11 21:15:44 +0900

    feat: 슬라이드별 필기 내용 기록 코드 추가

--- back/sockets/canvas.js
+++ back/sockets/canvas.js
@@ -1,7 +1,12 @@
 module.exports = function (io) {
   const SOCKET_TYPE = require("../constants/socket-type");
+  const Canvas = require("../app").CanvasInstance;
 
   io.on(SOCKET_TYPE.DRAW, (data) => {
+    const { slideId } = data;
+    delete data["slideId"];
+
+    Canvas.addDraw(slideId, data);
     io.broadcast.emit(SOCKET_TYPE.DRAW, data);
   });
 };
