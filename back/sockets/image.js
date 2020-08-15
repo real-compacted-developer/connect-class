95d2a811dc0d14590bf4b1a0b15fb9eaaa6657d3
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-08-15 13:22:50 +0900

    feat[Back]: 이미지 슬라이드 이전 버튼, 다음 버튼 기능 구현
    - roomNumber 라는 방 번호사람들에게 imageChange를 보낸다(emit).
    - 이미지는 S3에 올려논 더미데이터를 사용했다.
    - 나중에 데이터베이스에서 가져올 예정

--- back/sockets/image.js
+++ back/sockets/image.js
@@ -1,16 +1,17 @@
-const imagesPath = [
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat1.jpg",
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat2.jpg",
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat3.jpg",
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/puppy1.jpg"
-];
-const io = require("../bin/www").io;
-const size = imagesPath.length;
 module.exports = function (socket) {
   const SOCKET_TYPE = require("../constants/socket-type");
-
+  // const { Slide, StudyData } = require("../models/index");
+  // S3에 있는 더미 이미지 사용
+  const imagesPath = [
+    "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat1.jpg",
+    "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat2.jpg",
+    "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat3.jpg",
+    "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/puppy1.jpg"
+  ];
+  const io = require("../bin/www").io;
+  const size = imagesPath.length;
   socket.on(SOCKET_TYPE.IMAGE_PREV, (data) => {
-    // TODO: 자료 이전으로 넘기기
+    // TODO: 슬라이드 이미지 이전으로 넘기기
     if (data.index == 0) {
       data.index = size - 1;
     } else {
@@ -20,7 +21,7 @@ module.exports = function (socket) {
     io.in("roomNumber").emit("imageChange", data);
   });
   socket.on(SOCKET_TYPE.IMAGE_NEXT, (data) => {
-    // TODO: 자료 다음으로 넘기기
+    // TODO: 슬라이드 이미지 다음으로 넘기기
     if (data.index >= size - 1) {
       data.index = 0;
     } else {
