09eb1d24c5057c599e1f7fef1eaf16dc0cb6c73b
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-08-16 17:11:45 +0900

    feat[Back] : pdf를 업로드받아 각 슬라이드별로 slicing하는 기능
    - pdf를 front에서 업로드하면 그 pdf를 각 슬라이드 별로 이미지화하여 저장한다.

--- back/app.js
+++ back/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const usersRouter = require("./apis/users");
+const api = require("./apis");
 
 const RoomStore = require("./stores/RoomStore");
 const CanvasStore = require("./stores/CanvasStore");
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", usersRouter);
+app.use("/api", api);
 
 app.use(function (req, res, next) {
   next(createError(404));
