a96c2ae1211f2e9437c738143ef3ae7ed53a4b99
combbm <combbm@gmail.com>
2020-08-17 22:26:33 +0900

    feat: 스터디 생성 뷰 제목, 부제목 추가

--- front/src/pages/StudyCreatePage.tsx
+++ front/src/pages/StudyCreatePage.tsx
@@ -8,12 +8,52 @@ const Wrapper = styled.div`
   flex-direction: row;
 `;
 
+const BodyWrapper = styled.div`
+  flex-direction: column;
+`;
+
+const Body = styled.div`
+  width: 1609px;
+  margin: auto 312px;
+  margin-top: 65px;
+`;
+
+const Title = styled.h1`
+  font-family: "Gmarket Sans";
+  font-size: 30px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #34363b;
+`;
+
+const SubTitle = styled.p`
+  font-family: "Spoqa Han Sans";
+  font-size: 20px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #9a9fac;
+`;
+
 class StudyCreate extends React.Component {
   render() {
     return (
       <Wrapper>
         <CreateSideBar />
-        <Header />
+        <BodyWrapper>
+          <Header />
+          <Body>
+            <Title>스터디 개설하기</Title>
+            <SubTitle>만들고 싶은 스터디를 직접 만들고 참여해보세요.</SubTitle>
+          </Body>
+        </BodyWrapper>
       </Wrapper>
     );
   }
