a76d98c2d36b806e11492b493206e9a75b215f66
combbm <combbm@gmail.com>
2020-08-17 14:35:22 +0900

    feat: Add /study api

--- back/apis/index.js
+++ back/apis/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use("/user", require("./users"));
 router.use("/studyGroup", require("./studyGroups"));
+router.use("/study", require("./studyRoom"));
 
 module.exports = router;
