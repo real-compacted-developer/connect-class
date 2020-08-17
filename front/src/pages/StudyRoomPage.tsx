943e3d0bc197e15df7df89f946aaf59d8636f551
combbm <combbm@gmail.com>
2020-08-17 18:32:39 +0900

    feat: 뷰 로드 시 스터디룸 참가 코드 추가

--- front/src/pages/StudyRoomPage.tsx
+++ front/src/pages/StudyRoomPage.tsx
@@ -4,6 +4,9 @@ import styled from "styled-components";
 import QuestionList from "../components/StudyRoom/QuestionList";
 import SlideView from "../components/StudyRoom/SlideView";
 
+import { socket } from "..";
+import SOCKET_TYPE from "../constants/socket-type";
+
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
@@ -12,6 +15,14 @@ const Wrapper = styled.div`
 `;
 
 class StudyRoom extends React.Component {
+  componentDidMount() {
+    // TODO: 실제 방 ID하고 유저 ID 가져오는거 구현
+    socket.emit(SOCKET_TYPE.JOIN, {
+      roomId: 1,
+      userId: "사용자1",
+    });
+  }
+
   render() {
     return (
       <Wrapper>
