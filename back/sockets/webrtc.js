320e64afbff9f1b37634da9efb5d8bf144816fd7
changicho <changicho29@gmail.com>
2020-08-22 17:28:47 +0900

    feat: webRTC 소켓 통신부분 추가
    오디오 채팅을 위한 소켓 통신 (분기처리)를 추가함

--- back/sockets/webrtc.js
+++ back/sockets/webrtc.js
@@ -2,9 +2,10 @@ module.exports = function (socket) {
   const SOCKET_TYPE = require("../constants/socket-type");
 
   socket.on(SOCKET_TYPE.CREATE_OFFER, (data) => {
-    // TODO: WebRTC
+    socket.broadcast.emit(SOCKET_TYPE.TRANSMIT_OFFER, data);
   });
-  socket.on(SOCKET_TYPE.TRANSMIT_OFFER, (data) => {
-    // TODO: WebRTC
+
+  socket.on("askAudio", () => {
+    socket.broadcast.emit("shareAudioModal", "HARDCODING USER");
   });
 };
