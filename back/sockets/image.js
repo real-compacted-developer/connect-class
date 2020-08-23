71fb2cc7a85acb7f3f2a6020d5fde00323ef95a6
combbm <combbm@gmail.com>
2020-08-23 15:58:44 +0900

    fix: TODO 메세지 제거

--- back/sockets/image.js
+++ back/sockets/image.js
@@ -1,6 +1,6 @@
 const SOCKET_TYPE = require("../constants/socket-type");
-
 // const { Slide, StudyData } = require("../models/index");
+
 const imagesPath = [
   "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-01.png",
   "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-02.png",
@@ -20,31 +20,35 @@ const imagesPath = [
   "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-16.png",
   "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-17.png",
 ];
+
 const io = require("../bin/www").io;
 const size = imagesPath.length;
 let idx = 0;
 let url = imagesPath[0];
+
 module.exports = function (socket) {
   socket.emit(SOCKET_TYPE.SYNC, { idx, url });
+
   socket.on(SOCKET_TYPE.IMAGE_PREV, (data) => {
-    // TODO: 자료 이전으로 넘기기
     if (data.index == 0) {
       data.index = size - 1;
     } else {
       data.index -= 1;
     }
+
     data.urlInfo = imagesPath[data.index];
     idx = data.index;
     url = data.urlInfo;
     io.emit(SOCKET_TYPE.IMAGE_CHANGE, data);
   });
+
   socket.on(SOCKET_TYPE.IMAGE_NEXT, (data) => {
-    // TODO: 자료 다음으로 넘기기
     if (data.index >= size - 1) {
       data.index = 0;
     } else {
       data.index += 1;
     }
+
     data.urlInfo = imagesPath[data.index];
     idx = data.index;
     url = data.urlInfo;
