3c3910aa0eb733f20320368b4be8bf1d963b7ba2
combbm <combbm@gmail.com>
2020-08-23 16:52:45 +0900

    style: 4 tab에서 2 tab으로 정렬

--- front/src/components/StudyRoom/Slide/SlideCanvas.ts
+++ front/src/components/StudyRoom/Slide/SlideCanvas.ts
@@ -8,44 +8,44 @@ export const drawState = {
 };
 
 const sketch = (s: any) => {
-    s.setup = () => {
-      const cv = s.createCanvas(1000, 1000);
-      cv.id("Slide__canvas");
-      cv.parent("Slide__content");
+  s.setup = () => {
+    const cv = s.createCanvas(1000, 1000);
+    cv.id("Slide__canvas");
+    cv.parent("Slide__content");
 
-      socket.on(SOCKET_TYPE.DRAW, (data: Record<string, unknown>) => {
-        s.stroke(data.color);
-        s.strokeWeight(data.strokeWidth);
-        s.line(data.x, data.y, data.px, data.py);
-      });
+    socket.on(SOCKET_TYPE.DRAW, (data: Record<string, unknown>) => {
+      s.stroke(data.color);
+      s.strokeWeight(data.strokeWidth);
+      s.line(data.x, data.y, data.px, data.py);
+    });
+  };
+
+  s.mouseDragged = () => {
+    if (!drawState.isDraw) return;
+
+    s.stroke(drawState.color);
+    s.strokeWeight(4);
+    s.line(s.mouseX, s.mouseY, s.pmouseX, s.pmouseY);
+    sendDrawDataToServer(s.mouseX, s.mouseY, s.pmouseX, s.pmouseY);
+  };
+
+  const sendDrawDataToServer = (
+    x: number,
+    y: number,
+    pX: number,
+    pY: number
+  ) => {
+    const data = {
+      slideId: drawState.slideId,
+      x: x,
+      y: y,
+      px: pX,
+      py: pY,
+      color: drawState.color,
+      strokeWidth: 4,
     };
-
-    s.mouseDragged = () => {
-      if (!drawState.isDraw) return;
-
-      s.stroke(drawState.color);
-      s.strokeWeight(4);
-      s.line(s.mouseX, s.mouseY, s.pmouseX, s.pmouseY);
-      sendDrawDataToServer(s.mouseX, s.mouseY, s.pmouseX, s.pmouseY);
-    };
-
-    const sendDrawDataToServer = (
-      x: number,
-      y: number,
-      pX: number,
-      pY: number
-    ) => {
-      const data = {
-        slideId: drawState.slideId,
-        x: x,
-        y: y,
-        px: pX,
-        py: pY,
-        color: drawState.color,
-        strokeWidth: 4,
-      };
-      socket.emit(SOCKET_TYPE.DRAW, data);
-    };
+    socket.emit(SOCKET_TYPE.DRAW, data);
+  };
 };
 
 export default sketch;
