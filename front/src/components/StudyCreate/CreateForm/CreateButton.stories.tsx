2aef3083280c7ffda446dc02b889b486d05599fb
combbm <combbm@gmail.com>
2020-09-23 21:24:56 +0900

    fix: 사용하지 않는 코드 제거

--- front/src/components/StudyCreate/CreateForm/CreateButton.stories.tsx
+++ front/src/components/StudyCreate/CreateForm/CreateButton.stories.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
 import CreateButton from "./CreateButton";
 
 export default {
   title: "Components",
   component: CreateButton,
-  decorators: [withKnobs],
 };
 
 export const CreateButtonStory = () => {
