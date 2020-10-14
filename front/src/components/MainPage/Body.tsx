6c7ae917a45e13d2458ccb5f87ac3fd84cb0b08e
changicho <changicho29@gmail.com>
2020-10-14 20:03:53 +0900

    feat: 사이드바 제작
    사이드바 내부에 메뉴 컴포넌트를 추가해야하므로 별도의 폴더로 분리함
    
    메뉴 컴포넌트를 제작하고 사이드바에서 이용하도록 구성

--- front/src/components/MainPage/Body.tsx
+++ front/src/components/MainPage/Body.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import SideBar from "./SideBar";
+import SideBar from "./SideBar/SideBar";
 import Content from "./Content";
 
 const Wrapper = styled.div`
---
+++ front/src/components/MainPage/SideBar/Menu.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 30px;
+  margin: 21px 0;
+
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+`;
+
+const Icon = styled.div<{ url: string }>`
+  width: 30px;
+  height: 30px;
+
+  background-image: url(${(props) => props.url});
+`;
+
+const Text = styled.span`
+  margin-left: 20px;
+
+  height: 20px;
+  font-family: GmarketSans;
+  font-size: 20px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #9a9fac;
+
+  .active {
+    color: #346bff;
+  }
+`;
+
+type Props = {
+  iconURL: string;
+  text: string;
+};
+
+const Menu: (props: Props) => JSX.Element = (props) => {
+  return (
+    <Wrapper>
+      <Icon url={props.iconURL} />
+      <Text>{props.text}</Text>
+    </Wrapper>
+  );
+};
+
+export default Menu;
