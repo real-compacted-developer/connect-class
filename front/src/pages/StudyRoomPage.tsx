600537576c8eaad3b2f1931dd4c9025092621f09
combbm <combbm@gmail.com>
2020-10-21 18:01:24 +0900

    refactor: 클래스 컴포넌트에서 함수 컴포넌트로 변경

--- front/src/pages/StudyRoomPage.tsx
+++ front/src/pages/StudyRoomPage.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
-import { RouteComponentProps } from 'react-router-dom';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useRouteMatch } from "react-router-dom";
 
-import QuestionList from '../components/StudyRoom/QuestionList';
-import SlideView from '../components/StudyRoom/SlideView';
+import QuestionList from "../components/StudyRoom/QuestionList";
+import SlideView from "../components/StudyRoom/SlideView";
 
-import { socket } from '../index';
-import SOCKET_TYPE from '../constants/socket-type';
+import { socket } from "../index";
+import SOCKET_TYPE from "../constants/socket-type";
 
 const Wrapper = styled.div`
-	width: 100vw;
-	height: 100vh;
+  width: 100vw;
+  height: 100vh;
 
-	display: flex;
+  display: flex;
 `;
 
 type Params = {
-	id: string;
+  id: string;
 };
 
-class StudyRoom extends React.Component<RouteComponentProps<Params>> {
-	componentDidMount() {
-		const roomId = this.props.match.params.id;
-		// TODO: 실제 방 ID하고 유저 ID 가져오는거 구현
-		socket.emit(SOCKET_TYPE.JOIN, {
-			roomId: roomId,
-			userId: '사용자1',
-		});
-	}
+const StudyRoom: React.FC = () => {
+  const match = useRouteMatch<Params>();
 
-	render() {
-		return (
-			<Wrapper>
-				<SlideView></SlideView>
-				<QuestionList></QuestionList>
-			</Wrapper>
-		);
-	}
-}
+  useEffect(() => {
+    const roomId = match.params.id;
+    // TODO: 유저 ID 가져오는거 구현
+    socket.emit(SOCKET_TYPE.JOIN, {
+      roomId: roomId,
+      userId: "사용자1",
+    });
+  }, [match]);
+
+  return (
+    <Wrapper>
+      <SlideView></SlideView>
+      <QuestionList></QuestionList>
+    </Wrapper>
+  );
+};
 
 export default StudyRoom;
