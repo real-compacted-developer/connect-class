22c2e013bf37c3e6ff4cf99586eab2d21fd822dd
combbm <combbm@gmail.com>
2020-10-22 16:50:17 +0900

    feat: socket 관리용 redux state 추가

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import Router from "./router";
-import io from "socket.io-client";
-import config from "./config";
+
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
@@ -24,9 +23,7 @@ const index = (
   </React.StrictMode>
 );
 
-export const socket = io.connect(config.API);
-
-ReactDOM.render(index, document.getElementById("root"));
+export const socket = ReactDOM.render(index, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- front/src/modules/index.ts
+++ front/src/modules/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user";
+import socket from "./socket";
 
 const rootReducer = combineReducers({
   user,
+  socket,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
