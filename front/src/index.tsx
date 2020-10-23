ab0a1d5e82877c61d5529169a6b0bc4851c5d1a7
combbm <combbm@gmail.com>
2020-10-23 16:08:19 +0900

    feat: redux-thunk, redux-devtools-extension 적용

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import Router from "./router";
-
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import ReduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import "./stylesheets/main.css";
 
 const store = createStore(
   rootReducer,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(ReduxThunk))
 );
 
 const index = (
