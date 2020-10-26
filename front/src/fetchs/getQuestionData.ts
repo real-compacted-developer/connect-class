cc50376b54fbc9621e25fa1bec7e40de4acd2615
changicho <changicho29@gmail.com>
2020-10-26 19:24:14 +0900

    feat: 질문 데이터를 가져오는 fetch 함수 추가
    질문 Layer에서 질문 내용을 가져오는 fetch 함수를 추가함

--- front/src/fetchs/getQuestionData.ts
+++ front/src/fetchs/getQuestionData.ts
@@ -1,7 +1,25 @@
-import questionDummy from "../dummyDatas/QuestionDummy";
+import { IQuestionInfo } from "../types/question";
 
-async function getQuestionData(room: number) {
-  return questionDummy;
+// import questionDummy from "../dummyDatas/QuestionDummy";
+// async function getDummy(roomNumber: string): Promise<IQuestionInfo[]> {
+//   return questionDummy;
+// }
+
+async function getQuestionData(roomNumber: string): Promise<IQuestionInfo[]> {
+  const response = await fetch(
+    `${process.env.REACT_APP_QUESTION_LAYER}/study/${roomNumber}/questions`,
+    {
+      method: "GET",
+    }
+  ).then((res) => res.json());
+
+  console.log(response);
+
+  if (response.success === false) {
+    return [];
+  }
+
+  return response.data.questions;
 }
 
 export default getQuestionData;
