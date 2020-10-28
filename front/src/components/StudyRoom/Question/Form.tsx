d564896d1770fbead733ba3c890cee3ea7adefd5
changicho <changicho29@gmail.com>
2020-10-28 14:56:46 +0900

    feat: 질문 입력 컴포넌트(Form) 형식 변경
    ?: hooks로 사용하는 질문 리스트를 이용하기 위해 props형식 변경
    질문 추가시 hooks에서 가져오는 상태 변경 메소드를 이용해 state를 변경함
    상태 변화를 개발하고자 하므로 서버에 질문을 추가하는 로직은 주석처리

--- front/src/components/StudyRoom/Question/Form.tsx
+++ front/src/components/StudyRoom/Question/Form.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import createQuestionData from "../../../fetchs/createQuestionData";
+import { IQuestionInfo } from "../../../types/question";
 
 const Wrapper = styled.div`
   width: calc(100% - 104px);
@@ -49,34 +50,73 @@ const Button = styled.button`
   }
 `;
 
-const Form = (): JSX.Element => {
-  const [content, setContent] = useState<string>("");
+const dummySlide = {
+  slidePage: 1,
+  slideImageURL:
+    "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile7.uf.tistory.com%2Fimage%2F24283C3858F778CA2EFABE",
+};
+
+type Props = {
+  addQuestions: (data: IQuestionInfo) => void;
+};
+
+const Form = (props: Props): JSX.Element => {
   const roomNumber = window.location.pathname.split("/study/")[1];
 
+  const { addQuestions } = props;
+  const [content, setContent] = useState<string>("");
+
+  let inputTag: HTMLInputElement | null = null;
+
   const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    console.log(event.type, event.target.value);
+    // console.log(event.type, event.target.value);
     setContent(event.target.value);
   };
 
   const fetchCreate = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
+    if (content.length === 0) return;
+
     const data = {
       roomNumber: roomNumber,
       userId: "kakao1472290927",
-      slidePage: 1,
-      slideImageURL:
-        "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile7.uf.tistory.com%2Fimage%2F24283C3858F778CA2EFABE",
+      slidePage: dummySlide.slidePage,
+      slideImageURL: dummySlide.slideImageURL,
       title: "질문",
       content: content,
     };
-    createQuestionData(data);
+    // createQuestionData(data);
+
+    addQuestions({
+      slideInfo: {
+        page: dummySlide.slidePage,
+        imageURL: dummySlide.slideImageURL,
+      },
+      content: content,
+      userInfo: {
+        userName: "남궁권",
+        profileImageURL:
+          "http://k.kakaocdn.net/dn/CItX8/btqGvQ5a7B7/6IyKbRGltQPiyyfyS703VK/img_110x110.jpg",
+      },
+    });
+
+    if (inputTag) inputTag.value = "";
+    setContent("");
   };
 
   return (
     <Wrapper>
-      <Input placeholder="질문을 입력하세요" onChange={onChange} />
-      <Button onClick={fetchCreate}>질문하기</Button>
+      <Input
+        placeholder="질문을 입력하세요"
+        onChange={onChange}
+        ref={(element: HTMLInputElement | null) => {
+          inputTag = element;
+        }}
+      />
+      <Button type="reset" onClick={fetchCreate}>
+        질문하기
+      </Button>
     </Wrapper>
   );
 };
