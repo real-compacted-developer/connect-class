723d5edbe5e692599113b6017cd8430637af66c7
combbm <combbm@gmail.com>
2020-10-28 21:38:30 +0900

    feat: 스터디룸 목록 불러오는 기능 구현

--- front/src/components/MainPage/Content/StudyList.tsx
+++ front/src/components/MainPage/Content/StudyList.tsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-
 import StudyCard from "./StudyCard";
-
-import dummy from "../../../dummyDatas/StudyCardDummy";
+import Axios from "axios";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -34,18 +32,46 @@ const List = styled.ul`
   padding: 0;
 `;
 
+interface StudyGroupType {
+  readonly id: string;
+  readonly title: string;
+  readonly category: string;
+  readonly password: string;
+  readonly salt: string;
+  readonly people: string[];
+  readonly maxPeople: number;
+  readonly owner: string;
+  readonly isPremium: boolean;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+}
+
+const IT_IMAGE =
+  "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+
+const DEFAULT_IMAGE =
+  "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+
 const StudyList: React.FC = () => {
+  const [studyList, setStudyList] = useState<StudyGroupType[]>([]);
+
+  useEffect(() => {
+    Axios.get(`${process.env.REACT_APP_STUDY_LAYER}/group`).then((res) =>
+      setStudyList(res.data.data)
+    );
+  }, []);
+
   return (
     <Wrapper>
       <Title>스터디 목록</Title>
 
       <List>
-        {dummy.map((cur, index) => (
+        {studyList.map((cur, index) => (
           <StudyCard
             title={cur.title}
-            imageURL={cur.imageURL}
-            currentPeople={cur.currentPeople}
-            totalPeople={cur.totalPeople}
+            imageURL={cur.category === "IT분야" ? IT_IMAGE : DEFAULT_IMAGE}
+            currentPeople={0}
+            totalPeople={cur.maxPeople}
             key={`StudyCard:${index}`}
           />
         ))}
