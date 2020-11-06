f2936db58090427e21a3e40b893edb1903d3592b
kkoon9 <rndrnjs2003@gmail.com>
2020-11-06 13:51:00 +0900

    style : login 버튼 디자인
    - login 버튼 디자인 완료

---
+++ front/src/components/Login/Buttons/NaverButton.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export default styled.div`
+	width: 295px;
+	height: 61px;
+	border-radius: 35px;
+	background-color: #1fc604;
+	font-size: 18px;
+	font-weight: bold;
+	font-stretch: normal;
+	font-style: normal;
+	line-height: 61px;
+	letter-spacing: normal;
+	text-align: center;
+	color: #ffffff;
+`;
--- front/src/pages/LoginPage.tsx
+++ front/src/pages/LoginPage.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import Header from "../components/MainPage/Header";
-import Body from "../components/MainPage/Body";
 import dotenv from 'dotenv';
-import {KaKaoSignin} from '../components/Login/KaKaoSignin';
-import {GoogleSignin} from '../components/Login/googleSignin';
-import {Nav} from '../components/Login/Nav';
+import { KaKaoSignin } from '../components/Login/KaKaoSignin';
+import { GoogleSignin } from '../components/Login/GoogleSignin';
+import { Nav } from '../components/Login/Nav';
 import styled from 'styled-components';
 
 dotenv.config();
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
+	width: 100vw;
+	height: 100vh;
 `;
 const LoginPage: React.FC = () => {
 	// Redirect if logged in
@@ -20,11 +18,12 @@ const LoginPage: React.FC = () => {
 	}
 	return (
 		<Wrapper>
-          <Header />
 			<h1>Login Page</h1>
-      <KaKaoSignin />
-      <GoogleSignin/>
-      <Nav/>
+			<KaKaoSignin />
+			<br></br>
+			<GoogleSignin />
+			<br></br>
+			<Nav />
 		</Wrapper>
 	);
 };
