7a7e63d9a75962ff36892dfe1e8e74992332a114
combbm <combbm@gmail.com>
2020-11-10 15:00:26 +0900

    feat: PermissionRoute 적용

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,31 +1,49 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import StudyRoomPage from './pages/StudyRoomPage';
-import StudyCreatePage from './pages/StudyCreatePage';
-import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
-import StudyDataRoomPage from './pages/StudyDataRoomPage';
-import { useDispatch } from 'react-redux';
-import { fetchUserAsync } from './modules/user';
+import React, { useEffect } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import StudyRoomPage from "./pages/StudyRoomPage";
+import StudyCreatePage from "./pages/StudyCreatePage";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import StudyDataRoomPage from "./pages/StudyDataRoomPage";
+import { useDispatch } from "react-redux";
+import { fetchUserAsync } from "./modules/user";
+import PermissionRoute from "./components/common/PermissonRoute";
 
 const Router: React.FC = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(fetchUserAsync());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchUserAsync());
+  }, [dispatch]);
 
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path='/' component={MainPage} />
-				<Route exact path='/study/create' component={StudyCreatePage} />
-				<Route exact path='/dashboard/:id' component={StudyDataRoomPage} />
-				<Route exact path='/study/:id' component={StudyRoomPage} />
-				<Route exact path='/login' component={LoginPage} />
-			</Switch>
-		</BrowserRouter>
-	);
+  const LoginRedirect = () => <Redirect to="/login" />;
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <PermissionRoute
+          exact
+          path="/study/create"
+          success={StudyCreatePage}
+          failure={LoginRedirect}
+        />
+        <PermissionRoute
+          exact
+          path="/dashboard/:id"
+          success={StudyDataRoomPage}
+          failure={LoginRedirect}
+        />
+        <PermissionRoute
+          exact
+          path="/study/:id"
+          success={StudyRoomPage}
+          failure={LoginRedirect}
+        />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
