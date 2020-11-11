3d8424e8e01c833fa1dbace01d5d944c157529a9
combbm <combbm@gmail.com>
2020-11-11 22:12:54 +0900

    fix: auth layer 환경변수 잘못된 부분 수정

--- front/src/components/Login/GoogleSignin.tsx
+++ front/src/components/Login/GoogleSignin.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import GoogleLogin from 'react-google-login';
-import GoogleButton from './Buttons/GoogleButton';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import GoogleLogin from "react-google-login";
+import GoogleButton from "./Buttons/GoogleButton";
 
 export const GoogleSignin = () => {
   const history = useHistory();
   const responseGoogle = async (res: any) => {
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
 
     const data: Object = {
-      provider: 'google',
+      provider: "google",
       id: res.profileObj.googleId,
       nickname: res.profileObj.name,
       email: res.profileObj.email,
@@ -22,29 +22,39 @@ export const GoogleSignin = () => {
       isPremium: false,
     };
 
-    const userId = 'google' + res.profileObj.googleId;
-    const user = await axios.get(`${process.env.REACT_APP_DB_LAYER}/user/${userId}`);
+    const userId = "google" + res.profileObj.googleId;
+    const user = await axios.get(
+      `${process.env.REACT_APP_DB_LAYER}/user/${userId}`
+    );
     const isUser: string = JSON.stringify(user.data.success);
 
-    if (isUser === 'true') {
+    if (isUser === "true") {
       const token = await axios.post(
         `${process.env.REACT_APP_AUTH_LAYER}/token`,
         JSON.stringify(user.data.data),
         config
       );
-      localStorage.setItem('token', token.data.token);
+      localStorage.setItem("token", token.data.token);
       history.goBack();
     } else {
       try {
         const body = JSON.stringify(data);
-        const res = await axios.post(`${process.env.REACT_APP_DB_LAYER}/user`, body, config);
+        const res = await axios.post(
+          `${process.env.REACT_APP_DB_LAYER}/user`,
+          body,
+          config
+        );
         const msg: string = JSON.stringify(res.data.success);
-        if (msg === 'true') {
-          const token = await axios.post(`${process.env.REACT_APP_DB_LAYER}/token`, body, config);
-          localStorage.setItem('token', token.data.token);
+        if (msg === "true") {
+          const token = await axios.post(
+            `${process.env.REACT_APP_AUTH_LAYER}/token`,
+            body,
+            config
+          );
+          localStorage.setItem("token", token.data.token);
           history.goBack();
         } else {
-          alert('DB 오류입니다.');
+          alert("DB 오류입니다.");
         }
       } catch (err) {
         alert(err);
@@ -55,7 +65,10 @@ export const GoogleSignin = () => {
     <GoogleLogin
       clientId={process.env.REACT_APP_GOOGLE_KEY!}
       render={(renderProps) => (
-        <GoogleButton onClick={renderProps.onClick} disabled={renderProps.disabled}>
+        <GoogleButton
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
           구글로 시작하기
         </GoogleButton>
       )}
