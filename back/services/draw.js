3e7d1354002e04c8b182ffc1324dadbf90c0a1cf
combbm <combbm@gmail.com>
2020-11-13 23:27:58 +0900

    fix: 필기 불러오지 못하는 문제 해결

--- back/services/draw.js
+++ back/services/draw.js
@@ -2,18 +2,24 @@ const Canvas = require("../app").CanvasInstance;
 const SOCKET_TYPE = require("../constants/socket-type");
 
 function initStoredDrawData(socket, roomId, slideId, userId) {
-  const drawDataList = Canvas.getDrawData(roomId, slideId, userId);
+  const drawDataList = Object.values(Canvas.getSlideDrawData(roomId, slideId));
   if (drawDataList === undefined) return;
-  drawDataList.forEach((draw) => {
-    socket.emit(SOCKET_TYPE.DRAW, draw);
+  drawDataList.forEach((userDraw) => {
+    userDraw.forEach((draw) => {
+      socket.emit(SOCKET_TYPE.DRAW, draw);
+    });
   });
 }
 
 function sendStoredDrawData(roomId, slideId, userId) {
   const io = require("../bin/www").io;
-  const drawDataList = Canvas.getDrawData(roomId, slideId, userId);
+  const drawDataList = Object.values(Canvas.getSlideDrawData(roomId, slideId));
   if (drawDataList === undefined) return;
-  drawDataList.forEach((draw) => io.sockets.in(roomId).emit(SOCKET_TYPE.DRAW, draw));
+  drawDataList.forEach((userDraw) => {
+    userDraw.forEach((draw) => {
+      io.sockets.in(roomId).emit(SOCKET_TYPE.DRAW, draw);
+    });
+  });
 }
 
 exports.initStoredDrawData = initStoredDrawData;
